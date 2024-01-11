8945d34377f80aff6233dd4461ded35fc5539d28
Eric Hansson <erichansson.united@gmail.com>
2024-01-11 22:03:50 +0000

    Initial commit

---
+++ Token.cpp
@@ -0,0 +1,70 @@
+/*******************************************
+ * File: Token.cpp                         *
+ * Author: S. Blythe                       *
+ * Date: 12/2023                           *
+ * PURPOSE: implementation for Token       *
+ *******************************************/
+
+#include "Token.hpp"
+
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+// the promised global for string equivalents of TokenType enumeration
+string TokStr[]=
+{ "ERROR", "EOF_TOK", "NUM_INT", "NUM_REAL", "ADDOP", "MULOP", "ID", "RELOP", "ASSIGNOP", "LPAREN", "RPAREN",  "SEMICOLON",  "LBRACK", "RBRACK", "COMMA", "AND", "OR", "INTEGER", "FLOAT", "WHILE", "IF", "THEN", "ELSE", "VOID", "BEGIN", "END"};
+
+// This is a "list" of the keywords. Note that they are in the same order
+//   as found in the TokenType enumaration. 
+static string reserved[]={"int" , "float", "while", "if", "then", "else", "void", "begin", "end" };
+
+
+
+/******************************************************
+ *  just prints out the info describing this Token    *
+ *    to specified stream                             *
+ *                                                    *
+ *   os  - the stream to add the Token to             *
+ *                                                    *
+ *   returns: the updated stream                      *
+ ******************************************************/
+ostream&
+Token::print(ostream& os) const
+{
+  os
+     << "{ Type:"   << left << setw(10) << TokStr[_type] 
+     << " Value:"   << left << setw(10) << _value
+     << " Line Number:" << _line_num
+     << " }";
+  return os;
+}
+
+/******************************************************
+ *  Fills in information about this Token by reading  *
+ *    it from specified input stream                  *
+ *                                                    *
+ *   is  - the stream to read the Token from          *
+ *                                                    *
+ *   returns: nothing                                 *
+ *                                                    *
+ *     **** YOU MUST CODE THIS !!!!!! ****            *
+ ******************************************************/
+// Deterministic ... 2D-array 
+static int **DFA = nullptr;
+
+void Token::get(istream &is)
+{
+  // you must write this code !!!!
+    if (!DFA) { // if dfa tabel doesn't exist ... create one
+        DFA = new int*[END+1]; // Create the rows, which are the different states 
+        for (int state=ERROR; state<=END; state++) {
+            DFA[state] = new int[256]; // creates the columns, which are the characters
+            for (int ch=0; ch<256; ch++) {
+                DFA[state][ch] = ERROR; // initializes all the cells with error
+            }
+        }
+        
+    }
+}
